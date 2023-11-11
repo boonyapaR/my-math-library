23b8c2860831829b9c8e0bf974a54283cb1c4866
boonyapa radittirasut <bowrainbow2415@gmail.com>
2023-11-11 11:06:00 +0700

    Inital the math library

--- main.js
+++ main.js
@@ -14,6 +14,16 @@ function sum(a, b) {
   }
 }
 
+function sum(a, b) {
+  if (Array.isArray(a)) {
+    let sum = 0;
+    a.forEach((i) => (sum += i));
+    return sum;
+  } else if (typeof a === "number" && typeof b === "number") {
+    return a + b;
+  }
+}
+
 module.exports = {
   sum,
 };
